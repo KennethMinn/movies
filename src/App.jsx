0cb9c9967cb190ec10be05971a829e957f0349cc
KennethMinn <minthukha2472005@gmail.com>
2023-05-31 09:29:55 -0400

    fetched data

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDQ1ZjUyYmFjZTAwZDVmYWU1YWEyMjkyMzMxNDFkOCIsInN1YiI6IjY0Nzc0MjBjODlkOTdmMDExNjJiMDQyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cUhUXYoO_ck8fue5BsWO8vby6yqUMWZamSZnTzgCbuk",
+        },
+      };
+
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+          options
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>App</div>;
+};
+
+export default App;
